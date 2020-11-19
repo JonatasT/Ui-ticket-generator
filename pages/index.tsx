aaad1d1d59087f3aeb08f1ee188089634291d8e8
Jonatas da Silva Teixeira <jonatasteixeira@id.uff.br>
2020-11-18 22:20:17 -0300

    fix(index-page): exceed lines of code from code climate

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,25 @@
 import Image from "next/image";
 import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
 import Box from "@material-ui/core/Box";
 import FormControl from "@material-ui/core/FormControl";
 import Typography from "@material-ui/core/Typography";
 import useStyles from "../src/styles";
-
 import { Stepper } from "../src/components/Stepper";
-
 export default function Index() {
 	const classes = useStyles();
-
-	return (
-		<>
-			<Grid
-			container
-			direction="column"
-			justify="center"
-			alignItems="center"
-			>
-				<Box className={classes.box}>
-					<Box className={classes.innerBox}>
-						<FormControl>
-							<Image src="/logoColor.svg" alt="UI ticket generator" className={classes.logo} height={72} width={160}/>
-							
-							<Typography variant="h1" component="h1" className={classes.title}>
-								UI Ticket Generator
-							</Typography>
-							<Typography variant="h6" component="h2" className={classes.subTitle}>
-								Generate yourself a custom ticket for your event! Fast and easy. You can do it!
-							</Typography>
-
-							<Stepper/>
-						</FormControl>
-					</Box>
+	return (<Grid container direction="column" justify="center" alignItems="center">
+			 <Box className={classes.box}>
+				<Box className={classes.innerBox}>
+					<FormControl>
+						<Image src="/logoColor.svg" alt="UI ticket generator" className={classes.logo} height={72} width={160}/>
+						<Typography variant="h1" component="h1" className={classes.title}>UI Ticket Generator</Typography>
+						<Typography variant="h6" component="h2" className={classes.subTitle}>
+							Generate yourself a custom ticket for your event! Fast and easy. You can do it!
+						</Typography>
+						<Stepper/>
+					</FormControl>
 				</Box>
-			</Grid>
-		</>
+			</Box>
+		</Grid>
 	);
-} 
+}
