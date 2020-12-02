8d8f53910172f1d09e445c68e15a33e834c07700
Jonatas da Silva Teixeira <jonatasteixeira@id.uff.br>
2020-12-02 00:27:37 -0300

    chore: including props to action navigate

--- src/components/Buttons/NavigationSteps/index.tsx
+++ src/components/Buttons/NavigationSteps/index.tsx
@@ -1,23 +1,23 @@
-import { colors, IconButton } from '@material-ui/core';
+import { IconButton } from '@material-ui/core';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import { useStyles } from "./styles";
 
-export const NextStep = () => {
+export const NextStep = (props: any) => {
     const classes = useStyles();
 
     return (
-        <IconButton className={classes.iconButtons} color="primary" aria-label="Next step">
+        <IconButton onClick={props.action} className={classes.iconButtons} color="primary" aria-label="Next step">
             <ArrowForwardIcon/>
         </IconButton>
     );
 }
 
-export const PreviousStep = () => {
+export const PreviousStep = (props: any) => {
     const classes = useStyles();
     
     return (
-        <IconButton className={classes.iconButtons} color="primary" aria-label="Next step">
+        <IconButton onClick={props.action} className={classes.iconButtons} color="primary" aria-label="Next step">
             <ArrowBackIcon/>
         </IconButton>
     );
