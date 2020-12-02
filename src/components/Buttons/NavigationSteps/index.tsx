b561d3cad380651abfabe78c43d029fd07309eac
Jonatas da Silva Teixeira <jonatasteixeira@id.uff.br>
2020-12-02 00:22:26 -0300

    refactor: moving navigation buttons to buttons sub components

--- src/components/Buttons/NavigationSteps/index.tsx
+++ src/components/Buttons/NavigationSteps/index.tsx
@@ -1,18 +1,23 @@
-import { IconButton } from '@material-ui/core';
+import { colors, IconButton } from '@material-ui/core';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import { useStyles } from "./styles";
 
 export const NextStep = () => {
+    const classes = useStyles();
+
     return (
-        <IconButton color="primary" aria-label="Next step">
+        <IconButton className={classes.iconButtons} color="primary" aria-label="Next step">
             <ArrowForwardIcon/>
         </IconButton>
     );
 }
 
 export const PreviousStep = () => {
+    const classes = useStyles();
+    
     return (
-        <IconButton color="primary" aria-label="Next step">
+        <IconButton className={classes.iconButtons} color="primary" aria-label="Next step">
             <ArrowBackIcon/>
         </IconButton>
     );
---
+++ src/components/Buttons/NavigationSteps/styles.tsx
@@ -0,0 +1,8 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export const useStyles = makeStyles({
+    iconButtons: {
+        scale: .8,
+        marginRight: 1,
+    },
+});
