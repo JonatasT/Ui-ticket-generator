dc6cec6ef42e481b42961a87d9b72a416600b4ae
Jonatas da Silva Teixeira <jonatasteixeira@id.uff.br>
2020-12-06 23:37:33 -0300

    chore: updating favicon icon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,12 @@ export default class MyDocument extends Document {
         <Head>
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"/>
+          <link rel="manifest" href="/favicon/site.webmanifest"/>
+          <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5"/>
+          <meta name="msapplication-TileColor" content="#da532c"/>
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
