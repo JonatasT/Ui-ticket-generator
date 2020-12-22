427c826c84ff8276195d738aa02c47ff82bf1e89
Jonatas Teixeira <jonatasteixeira@pop-os.localdomain>
2020-12-21 23:31:50 -0300

    fix: navigation button distance

--- src/components/Buttons/NavigationSteps/styles.tsx
+++ src/components/Buttons/NavigationSteps/styles.tsx
@@ -2,7 +2,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles({
     iconButtons: {
-        scale: .8,
-        marginRight: 1,
+        scale: .1,
+        marginRight: 15,
     },
 });
