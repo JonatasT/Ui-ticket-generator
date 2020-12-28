94be213c56a0392b0ca91d2a06d838fea4ddec99
Jonatas da Silva Teixeira <jonatasteixeira@id.uff.br>
2020-12-27 23:18:29 -0300

    feat: including state management into the pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,21 +3,28 @@ import type { AppProps } from "next/app";
 import PropTypes from "prop-types";
 import Head from "next/head";
 import ThemeContainer from "../contexts/theme/ThemeContainer";
+import { StateMachineProvider, createStore } from "little-state-machine";
+
+createStore({
+  data: {}
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <React.Fragment>
-      <Head>
-        <title>UI Ticket Generator</title>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
-        />
-      </Head>
-      <ThemeContainer >
-        <Component {...pageProps} />
-      </ThemeContainer>
-    </React.Fragment>
+    <StateMachineProvider>
+      <React.Fragment>
+        <Head>
+          <title>UI Ticket Generator</title>
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width"
+          />
+        </Head>
+        <ThemeContainer >
+          <Component {...pageProps} />
+        </ThemeContainer>
+      </React.Fragment>
+    </StateMachineProvider>
   );
 }
 
