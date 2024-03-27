15fe5105365dd7a49fa122a132100f3e65f643c1
Jonatas Teixeira <jonatasteixeira@id.uff.br>
2024-03-26 23:40:51 -0300

    Update index.tsx
    Adding missing property

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ export default function Index() {
 			 <Box marginTop={15} className={classes.box}>
 				<Box className={classes.innerBox}>
 					<FormControl>
-						<Image src="/logoColor.svg" alt="UI ticket generator" className={classes.logo} height={72} width={160}/>
+						<Image src="/logoColor.svg" alt="UI ticket generator" className={classes.logo} height={72} width={160} alt=""/>
 						<Typography variant="h1" component="h1" className={classes.title}>UI Ticket Generator</Typography>
 						<Typography variant="h6" component="h2" className={classes.subTitle}>
 							Generate yourself a custom ticket for your event! Fast and easy. You can do it!
